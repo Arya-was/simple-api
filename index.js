67a2f5d574fc242c232d5c04038361ae6829631c
Loli Protector <74448616+Arya-was@users.noreply.github.com>
2021-09-03 17:20:18 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express'),
+    secure = require('ssl-express-www')
+const PORT = process.env.PORT || 8080;
+const morgan = require('morgan')
+var app = express();
+app.use(secure)
+app.use(morgan('dev'));
+app.use(express.static('client'));
+__path = process.cwd()
+
+var downloader = require('./server/downloader.js');
+var search = require('./server/search.js');
+var randomimg = require('./server/randomimage.js');
+var main = require('./main');
+
+app.use('/downloader', downloader)
+app.use('/search', search)
+app.use('/randomimg', randomimg)
+app.use('/', main)
+
+
+app.listen(PORT, () => {
+    console.log(`Server Run on port ${PORT}`)
+});
