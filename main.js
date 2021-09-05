f56c39a7251a09b8a7f88a179e6f08a66ed179f4
Loli Protector <74448616+Arya-was@users.noreply.github.com>
2021-09-05 14:15:00 +0700

    Update main.js

--- main.js
+++ main.js
@@ -4,19 +4,10 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', async(req, res) => {
-	const config = `{
-		tiktok: "/downloader/tiktok?link=https://vm.tiktok.com/ZSJcLPNpe/",
-		mediaFire: "/downloader/mediafireDl?link=https://www.mediafire.com/file/pwxob70rpgma9lz/GBWhatsApp_v8.75%2528Tutorial_Yud%2529.apk/file",
-		igdl: "/downloader/igdl?link=https://www.instagram.com/p/CTEDAEbpE4A/?utm_source=ig_web_copy_link",
-		waifu_image: "/randomimg/waifu",
-		cosplay_image: "/randomimg/cosplay",
-		husbu_image: "/randomimg/husbu",
-		loli_image: "/randomimg/loli",
-		milf_image: "/randomimg/milf",
-		pinterest: "/search/pinterest?query=anime",
-		google: "/search/google?query=anime"
-	}`
-	res.send(config)
+	res.sendFile(__path + '/views/index.html')
+})
+router.get('/css/style.css', async(req, res) => {
+	res.sendFile(__path + '/views/css/style.css')
 })
 
 //Kalo page yang di cari engga ada, nanti muncul ini:v
