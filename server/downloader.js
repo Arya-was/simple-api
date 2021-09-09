a9eb54ded1500894e55236cb9a7ab54ba9bc6dcc
Loli Protector <74448616+Arya-was@users.noreply.github.com>
2021-09-09 11:04:55 +0700

    Update twitter & youtube

--- server/downloader.js
+++ server/downloader.js
@@ -1,5 +1,6 @@
 const express = require('express')
 var router = express.Router();
+const hxz = require('hxz-api')
 
 //scraper
 const { igDownload, tiktok, mediafireDl } = require('../scraper/index') 
@@ -30,6 +31,28 @@ router.get('/mediafireDl', async(req, res) => {
 	var link = req.query.link
 	if (!link) return res.json({ message: 'masukan parameter Link' })
 	var hasil = await mediafireDl(link)
+	try {
+		res.json(hasil)
+	} catch(err) {
+		console.log(err)
+		res.json({ message: 'Ups, error' })
+	}
+})
+router.get('/youtube', async(req, res) => {
+	var link = req.query.link
+	if (!link) return res.json({ message: 'masukan parameter Link' })
+	var hasil = await hxz.youtube(link)
+	try {
+		res.json(hasil)
+	} catch(err) {
+		console.log(err)
+		res.json({ message: 'Ups, error' })
+	}
+})
+router.get('/twitter', async(req, res) => {
+	var link = req.query.link
+	if (!link) return res.json({ message: 'masukan parameter Link' })
+	var hasil = await hxz.twitter(link)
 	try {
 		res.json(hasil)
 	} catch(err) {
