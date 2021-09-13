cee5eb95b11d166ff8a22b91c315c71c89e8f993
Loli Protector <74448616+Arya-was@users.noreply.github.com>
2021-09-13 16:41:04 +0700

    Update:v

---
+++ server/nsfw.js
@@ -0,0 +1,20 @@
+const express = require('express')
+var router = express.Router();
+
+var nhentai = require('nhentai-js');
+const NanaAPI = require('nana-api')
+const nana = new NanaAPI()
+
+router.get('/nHentai', async (req, res) => {
+        var code = req.query.code
+        var result = await nhentai.getDoujin(`${code}`)
+		    res.json({ result })
+})
+router.get('/nHentaiSearch', async (req, res) => {
+        var query = req.query.query
+        var hasil = await nana.search(`${query}`)
+        var result = hasil.results
+		    res.json({ result })
+})
+
+module.exports = router
