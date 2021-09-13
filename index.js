ea3575d819c33c3ba73d7d017b7a8cb3588669ed
Loli Protector <74448616+Arya-was@users.noreply.github.com>
2021-09-13 16:42:35 +0700

    Update index.js

--- index.js
+++ index.js
@@ -11,11 +11,13 @@ __path = process.cwd()
 var downloader = require('./server/downloader.js');
 var search = require('./server/search.js');
 var randomimg = require('./server/randomimage.js');
+var nsfw = require('./server/nsfw.js');
 var main = require('./main');
 
 app.use('/downloader', downloader)
 app.use('/search', search)
 app.use('/randomimg', randomimg)
+app.use('/nsfw', nsfw)
 app.use('/', main)
 
 
