8c07850c635de07330aba155fcee29dd101f54d9
Loli Protector <74448616+Arya-was@users.noreply.github.com>
2021-09-16 20:53:58 +0700

    Create pinterestdl.js

---
+++ scraper/pinterestdl.js
@@ -0,0 +1,20 @@
+const fetch = require('node-fetch')
+const cheerio = require('cheerio')
+
+function pinterestdl(url) {
+    return new Promise((resolve, reject) => {
+        let params = new URLSearchParams()
+        params.append('url', url)
+        fetch('https://www.expertsphp.com/facebook-video-downloader.php', { method: 'POST', body: params })
+            .then(res => res.text())
+            .then(res => {
+                const $ = cheerio.load(res)
+                var img = $('#showdata > div:nth-child(5) > table > tbody > tr:nth-child(2) > td:nth-child(1) > a').attr('href')
+                var vid = $('#showdata > div:nth-child(5) > table > tbody > tr:nth-child(1) > td:nth-child(1) > a').attr('href')
+                const result = { image: img, video: vid }
+                resolve({ data })
+            }).catch(reject)
+    })
+}
+
+module.exports = pinterestdl
