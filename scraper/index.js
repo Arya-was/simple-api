e22dd98524b9888937bfcedf8f89476d82a73d33
Loli Protector <74448616+Arya-was@users.noreply.github.com>
2021-09-18 11:33:31 +0700

    Update index.js

--- scraper/index.js
+++ scraper/index.js
@@ -2,5 +2,6 @@ module.exports = {
 	tiktok: require('./tiktok.js'),
 	pinterest: require('./pinterest'),
 	igDownload: require('./igdl'),
-	mediafireDl: require('./mediafire')
+	mediafireDl: require('./mediafire'),
+	doujindesu: require('./doujindesu')
 }
