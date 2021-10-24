d8fedc3b4bea87db94a8c29bffc1c2f83c1980c5
Loli Protector <74448616+Arya-was@users.noreply.github.com>
2021-10-24 13:31:31 +0700

    yaudah apdet

--- scraper/tiktok.js
+++ scraper/tiktok.js
@@ -1,9 +1,16 @@
+const tiktok_scrape = require('tiktok-scraper')
 const axios = require('axios')
 const cheerio = require('cheerio')
+const fetch = require('node-fetch')
 
-async function tiktok (Url) {
+const headers = {
+	"user-agent": "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36",
+	"referer": "https://www.tiktok.com/",
+	"cookie": "tt_webid_v2=6976572065375626290; ttwid=1|WphZXEIq_oTph3Rcy7Ybj7o2YmzOAhfdWm23L35UL7Q|1634565622|26309b01156f00c630959974d6ff872e1c6b5d6f6275cbe3aac6738e4f6d50f5; tt_webid=6976572065375626290; tt_csrf_token=KOCPsah2eI6vhK6-F_73Uben; cmpl_token=AgQQAPOgF-RMpbF7dxi_PJ07-rBaZ9U_nbArYPwp_A"
+}
+
+async function noWM (Url) {
 	return new Promise (async (resolve, reject) => {
-		const asu = await axios.get(`https://qload.info/tiktok-audio?link=${Url}`)
 		await axios.request({
 			url: "https://ttdownloader.com/",
 			method: "GET",
@@ -28,20 +35,33 @@ async function tiktok (Url) {
 					"cookie": "_ga=GA1.2.1240046717.1620835673; PHPSESSID=i14curq5t8omcljj1hlle52762; popCookie=1; _gid=GA1.2.1936694796.1623913934"
 				}
 			}).then(res => {
-				const SSS = cheerio.load(asu.data)
 				const ch = cheerio.load(res.data)
-				const result = {
-					status: res.status,
-					result: {
-						nowatermark: ch('#results-list > div:nth-child(2)').find('div.download > a').attr('href'),
-						watermark: ch('#results-list > div:nth-child(3)').find('div.download > a').attr('href'),
-						audio: SSS('#download-track-btn').attr('href')
-					}
-				}
+				const result = {}
+				result.videoUrl = ch('#results-list > div:nth-child(3)').find('div.download > a').attr('href')				
+				result.nowatermark = ch('#results-list > div:nth-child(2)').find('div.download > a').attr('href')				
+				result.music = ch('#results-list > div:nth-child(4)').find('div.download > a').attr('href')
 				resolve(result)
 			}).catch(reject)
 		}).catch(reject)
 	})
 }
+async function tiktok(url) { 
+var fullUrl = await fetch(url)
+const tiktok = await tiktok_scrape.getVideoMeta(fullUrl.url, {headers})
+const tiktok_dl = await noWM(url)
+const obj = {}
+obj.id = tiktok.collector[0].id
+obj.desctription = tiktok.collector[0].text 
+obj.thumbnail = tiktok.collector[0].imageUrl
+obj.likes = tiktok.collector[0].diggCount
+obj.share = tiktok.collector[0].shareCount
+obj.comment = tiktok.collector[0].commentCount
+obj.view = tiktok.collector[0].playCount
+obj.music_name = tiktok.collector[0].musicMeta.musicName
+obj.videoUrl = tiktok_dl.videoUrl
+obj.nowatermark = tiktok_dl.nowatermark
+obj.music = tiktok_dl.music
+return obj
+}
 
-module.exports = tiktok
+module.exports = tiktok 
