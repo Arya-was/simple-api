e8a06036dd869bf2bc1667ea32a20f7d80342d2b
Loli Protector <74448616+Arya-was@users.noreply.github.com>
2021-10-28 09:57:11 +0700

    Update photooxy.js

--- server/photooxy.js
+++ server/photooxy.js
@@ -6,34 +6,34 @@ const fs = require('fs')
 
 const { photoXy_1, photoXy_2 } = require('../scraper/photooxy')
 
-router.get('/photooxy1', async(req, res) => {
+router.get('/photooxy1', async (req, res) => {
 	var text = req.query.text
-  var link = req.query.link
+  	var link = req.query.link
 	if (!text) return res.json({ message: 'masukan parameter Text' })
-  if (!link) return res.json({ message: 'masukan parameter Link' })
+  	if (!link) return res.json({ message: 'masukan parameter Link' })
 	var hasil = await photoXy_1(link, text)
 	try {
-		var Buffer = await (await fetch(hasil)).buffer()
-    fs.writeFileSync('../tmp/image.jpg')
-    res.sendFile(__path + '/tmp/image.jpg')
+	var Buffer = await (await fetch(hasil)).buffer()
+    	await fs.writeFileSync('../tmp/image.jpg')
+   	 res.sendFile(__path + '/tmp/image.jpg')
 	} catch(err) {
 		console.log(err)
 		res.json({ message: 'Ups, error' })
 	}
 })
 
-router.get('/photooxy2', async(req, res) => {
+router.get('/photooxy2', async (req, res) => {
 	var text = req.query.text
-  var text_2 = req.query.text_2
-  var link = req.query.link
+  	var text_2 = req.query.text_2
+  	var link = req.query.link
 	if (!text) return res.json({ message: 'masukan parameter Text' })
-  if (!text_2) return res.json({ message: 'masukan parameter Text' })
-  if (!link) return res.json({ message: 'masukan parameter Link' })
-	var hasil = await photoXy_1(link, text, text_2)
+  	if (!text_2) return res.json({ message: 'masukan parameter Text' })
+  	if (!link) return res.json({ message: 'masukan parameter Link' })
+	var hasil = await photoXy_2(link, text, text_2)
 	try {
-		var Buffer = await (await fetch(hasil)).buffer()
-    fs.writeFileSync('../tmp/image.jpg')
-    res.sendFile(__path + '/tmp/image.jpg')
+	var Buffer = await (await fetch(hasil)).buffer()
+    	await fs.writeFileSync('../tmp/image.jpg')
+   	res.sendFile(__path + '/tmp/image.jpg')
 	} catch(err) {
 		console.log(err)
 		res.json({ message: 'Ups, error' })
