a46beeb94c949611eb1fc6e12bbd5832ae2e4675
Loli Protector <74448616+Arya-was@users.noreply.github.com>
2021-10-29 13:12:16 +0700

    add TIKTOK SEARCH

--- server/search.js
+++ server/search.js
@@ -2,7 +2,7 @@ const express = require('express')
 var router = express.Router();
 
 //scraper
-const { pinterest } = require('../scraper/index') 
+const { pinterest, randomTiktok } = require('../scraper/index') 
 
 router.get('/google', async(req, res) => {
 	var query = req.query.query
@@ -16,6 +16,12 @@ router.get('/pinterest', async(req, res) => {
 	var query = req.query.query
 	if (!query) return res.json({ message: 'masukan parameter query' })
 	var result = await pinterest(query)
+	res.json({ result })
+})
+router.get('/tiktok', async(req, res) => {
+	var query = req.query.query
+	if (!query) return res.json({ message: 'masukan parameter query' })
+	var result = await randomTiktok(query)
 	res.json({ result })
 })
 
