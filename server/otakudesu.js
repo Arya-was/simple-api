d23a72482fbd1f966242b19735e4e7a2d67c383c
Loli Protector <74448616+Arya-was@users.noreply.github.com>
2021-10-30 22:44:50 +0700

    Update Otakudesu

---
+++ server/otakudesu.js
@@ -0,0 +1,42 @@
+const express = require('express')
+var router = express.Router();
+
+const otaku = require('../scraper/otakudesu')
+
+router.get('/search', async(req, res) => {
+	var link = req.query.link
+	if (!link) return res.json({ message: 'masukan parameter Link' })
+	var hasil = await otaku.Search(link)
+	try {
+		res.json(hasil)
+	} catch(err) {
+		console.log(err)
+		res.json({ message: 'Ups, error' })
+	}
+})
+
+router.get('/animeDetail', async(req, res) => {
+	var link = req.query.link
+	if (!link) return res.json({ message: 'masukan parameter Link' })
+	var hasil = await otaku.getInfo(link)
+	try {
+		res.json(hasil)
+	} catch(err) {
+		console.log(err)
+		res.json({ message: 'Ups, error' })
+	}
+})
+
+router.get('/dl', async(req, res) => {
+	var link = req.query.link
+	if (!link) return res.json({ message: 'masukan parameter Link' })
+	var hasil = await otaku.Getdownload(link)
+	try {
+		res.json(hasil)
+	} catch(err) {
+		console.log(err)
+		res.json({ message: 'Ups, error' })
+	}
+})
+
+module.exports = router
