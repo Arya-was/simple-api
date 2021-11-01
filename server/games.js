1ce72d978f488b3b3fc483af62e8ca728df4ecd6
Loli Protector <74448616+Arya-was@users.noreply.github.com>
2021-11-01 13:15:49 +0700

    Create games.js

---
+++ server/games.js
@@ -0,0 +1,16 @@
+const express = require('express')
+var router = express.Router();
+
+const { tebakgambar } = require('../scraper/index') 
+
+router.get('/tebakgambar', async(res) => {
+	var hasil = await tebakgambar()
+	try {
+		res.json(hasil)
+	} catch(err) {
+		console.log(err)
+		res.json({ message: 'Ups, error' })
+	}
+})
+
+module.exports = router
