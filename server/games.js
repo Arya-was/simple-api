6c467669c4541ade70128361991ffd250b17378b
Loli Protector <74448616+Arya-was@users.noreply.github.com>
2021-11-01 13:22:10 +0700

    Update games.js

--- server/games.js
+++ server/games.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 const { tebakgambar } = require('../scraper/index') 
 
-router.get('/tebakgambar', async(res) => {
+router.get('/tebakgambar', async(req, res) => {
 	var hasil = await tebakgambar()
 	try {
 		res.json(hasil)
