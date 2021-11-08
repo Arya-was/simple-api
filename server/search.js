6fcb367f1c59bebfefc213f8f4612330f63ee684
Loli Protector <74448616+Arya-was@users.noreply.github.com>
2021-11-08 22:56:16 +0700

    Update search.js

--- server/search.js
+++ server/search.js
@@ -1,5 +1,8 @@
 const express = require('express')
 var router = express.Router();
+__path = process.cwd()
+const fs = require('fs')
+const { getBuffer } = require('../lib/function')
 
 //scraper
 const { pinterest, randomTiktok, konachan } = require('../scraper/index') 
@@ -22,8 +25,11 @@ router.get('/pinterest', async(req, res) => {
 router.get('/konachan', async(req, res) => {
 	var query = req.query.query
 	if (!query) return res.json({ message: 'masukan parameter query' })
-	var result = await konachan(query)
-	res.json({ result })
+	var img = (await konachan(query)).result
+	const result = img[Math.floor(Math.random() * (img.length))]
+	var data = await getBuffer(result)
+    	await fs.writeFileSync(__path +'/tmp/konachan.png', data)
+   	await res.sendFile(__path +'/tmp/konachan.png')
 })
 router.get('/tiktok', async(req, res) => {
 	var query = req.query.query
