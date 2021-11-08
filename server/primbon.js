d8979e49061647c1132070f125906312359ea8d8
Loli Protector <74448616+Arya-was@users.noreply.github.com>
2021-11-08 23:31:26 +0700

    Create primbon.js

---
+++ server/primbon.js
@@ -0,0 +1,32 @@
+const express = require('express')
+var router = express.Router();
+
+const { artinama, ramalanJodoh } = require('../scraper/primbon')
+
+router.get('/artinama', async(req, res) => {
+	var nama = req.query.nama
+	if (!nama) return res.json({ message: 'masukan parameter nama' })
+	var hasil = await artinama(nama)
+	try {
+		res.json(hasil)
+	} catch(err) {
+		console.log(err)
+		res.json({ message: 'Ups, error' })
+	}
+})
+
+router.get('/ramalanjodoh', async(req, res) => {
+	var nama = req.query.nama
+  var pasangan = req.query.pasangan
+	if (!nama) return res.json({ message: 'masukan parameter nama' })
+  if (!pasangan) return res.json({ message: 'masukan parameter pasangan' })
+	var hasil = await artinama(nama, pasangan)
+	try {
+		res.json(hasil)
+	} catch(err) {
+		console.log(err)
+		res.json({ message: 'Ups, error' })
+	}
+})
+
+module.exports = router
