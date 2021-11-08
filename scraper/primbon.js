d0c2265a0c176baf1bb6e0cbbdb3fa1d03b571dc
Loli Protector <74448616+Arya-was@users.noreply.github.com>
2021-11-08 22:39:11 +0700

    Update primbon.js

--- scraper/primbon.js
+++ scraper/primbon.js
@@ -4,11 +4,11 @@ const cheerio = require('cheerio');
 function artinama(nama) {
 	return new Promise((resolve, reject) => {
 		axios.get('http://www.primbon.com/arti_nama.php?nama1='+nama+'&proses=+Submit%21+').then(res => {
-			const $ = cheerio.load(res.data)
-			const r = $('#body').text();
-				const re = r.split('\n      \n        \n        \n')[0]
-				const result = re.trim()
-			console.log(result)
+		const $ = cheerio.load(res.data)
+		const r = $('#body').text();
+		const re = r.split('\n      \n        \n        \n')[0]
+		const result = re.trim()
+		resolve(result)
 		})
 	})
 }
@@ -16,9 +16,9 @@ function artinama(nama) {
 function ramalanJodoh(nama, pasangan) {
 	return new Promise((resolve, reject) => {
 		axios.get('https://www.primbon.com/kecocokan_nama_pasangan.php?nama1='+nama+'&nama2='+pasangan+'&proses=+Submit%21+').then(res => {
-			const $ = cheerio.load(res.data)
-			const thumb = 'https://www.primbon.com/'+$('#body > img').attr('src')
-			const isi = $('#body').text().split(pasangan)[1].replace('< Hitung Kembali','').split('\n')[0]
+		const $ = cheerio.load(res.data)
+		const thumb = 'https://www.primbon.com/'+$('#body > img').attr('src')
+		const isi = $('#body').text().split(pasangan)[1].replace('< Hitung Kembali','').split('\n')[0]
       		const positif = isi.split('Sisi Negatif Anda: ')[0].replace('Sisi Positif Anda: ','')
       		const negatif = isi.split('Sisi Negatif Anda: ')[1]
       		const result = {
@@ -27,7 +27,6 @@ function ramalanJodoh(nama, pasangan) {
       			negatif: negatif
       		}
       		resolve(result)
-      		console.log(result)
 		})
 
 	})
