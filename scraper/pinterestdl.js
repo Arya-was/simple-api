c904ef954dd040fb78cc2d6818b64051280f33a0
Loli Protector <74448616+Arya-was@users.noreply.github.com>
2021-11-08 23:16:49 +0700

    FIX

--- scraper/pinterestdl.js
+++ scraper/pinterestdl.js
@@ -1,20 +1,24 @@
-const fetch = require('node-fetch')
+const axios = require('axios')
 const cheerio = require('cheerio')
                    
 function pinterestdl(url) {
     return new Promise((resolve, reject) => {
-        let params = new URLSearchParams()
-        params.append('url', url)
-        fetch('https://www.expertsphp.com/facebook-video-downloader.php', { method: 'POST', body: params })
-            .then(res => res.text())
-            .then(res => {
-                const $ = cheerio.load(res)
-                var img = $('#showdata > div:nth-child(5) > table > tbody > tr:nth-child(2) > td:nth-child(1) > a').attr('href')
-                var vid = $('#showdata > div:nth-child(5) > table > tbody > tr:nth-child(1) > td:nth-child(1) > a').attr('href')
-                const result = { img, vid }
+        axios.request({
+            url: 'https://www.expertsphp.com/facebook-video-downloader.php',
+            method: "POST",
+            data: new URLSearchParams(Object.entries({url: url})),
+            headers: {
+                "content-type": "application/x-www-form-urlencoded",
+                "user-agent": "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36"
+            }
+        }).then(res => {
+            const $ = cheerio.load(res.data)
+            const img = $('#showdata > div:nth-child(4) > table > tbody > tr:nth-child(2) > td:nth-child(1) > a').attr('href')
+            const vid = $('#showdata > div:nth-child(4) > table > tbody > tr:nth-child(1) > td:nth-child(1) > a').attr('href')
+            const result = { img, vid }
                 if (typeof vid == 'undefined') return resolve({ result: img })
                 resolve({ result: vid })
-            }).catch(reject)
+        })
     })
 }
 
