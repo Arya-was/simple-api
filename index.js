a8ef768a7455faeca126ae88a7edc1391e36d395
Loli Protector <74448616+Arya-was@users.noreply.github.com>
2021-11-10 20:49:13 +0700

    Update index.js

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ var photooxy = require('./server/photooxy.js');
 var otakudesu = require('./server/otakudesu.js');
 var games = require('./server/games.js');
 var primbon = require('./server/primbon.js');
+var convert = require('./server/convert.js');
 var main = require('./main');
 
 app.use('/downloader', downloader)
@@ -31,6 +32,7 @@ app.use('/photooxy', photooxy)
 app.use('/otakudesu', otakudesu)
 app.use('/games', games)
 app.use('/primbon', primbon)
+app.use('converter', convert)
 app.use('/', main)
 
 // Cretae folder
