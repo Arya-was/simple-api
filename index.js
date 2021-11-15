207ca0167f101be920e5bdc4ec153bdc68acf026
Loli Protector <74448616+Arya-was@users.noreply.github.com>
2021-11-15 10:12:43 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express'),
-    secure = require('ssl-express-www')
+const secure = require('ssl-express-www')
 const PORT = process.env.PORT || 8080;
 const morgan = require('morgan')
 const multer = require('multer')
@@ -36,56 +36,6 @@ app.use('/primbon', primbon)
 app.use('/converter', convert)
 app.use('/other', other)
 app.use('/', main)
-
-// Cretae folder
-if (!fs.existsSync('./tmp/file')) fs.mkdirSync('./tmp/file')
-
-    function makeid(length) {
-    let result = '';
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    const charactersLength = characters.length;
-    for (let i = 0; i < length; i++) {
-        result += characters.charAt(Math.floor(Math.random() *
-            charactersLength));
-    }
-    return result;
-}
-
-const storage = multer.diskStorage({
-    destination: 'tmp/file',
-    filename: (req, file, cb) => {
-        cb(null, makeid(12) +
-            path.extname(file.originalname))
-    }
-});
-
-const upload = multer({
-    storage,
-    limits: {
-        fileSize: 10000000 // 10 MB
-    }
-})
-
-app.post('/upload', upload.single('file'), (req, res) => {
-    if (!req.file.path) return res.status(400).json({
-        status: false,
-        message: "No file uploaded"
-    })
-    res.status(200).json({
-        status: true,
-        result: {
-            originalname: req.file.originalname,
-            encoding: req.file.encoding,
-            mimetype: req.file.mimetype,
-            size: req.file.size,
-            url: "https://" + req.hostname + "/file/" + req.file.filename
-        }
-    })
-}, (error, req, res, next) => {
-    res.status(400).json({
-        error: error.message
-    })
-})
 
 app.listen(PORT, () => {
     console.log(`Server Run on port ${PORT}`)
