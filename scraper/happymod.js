f3db52f8688c13f82bb6a52180f6412afd755e4e
Loli Protector <74448616+Arya-was@users.noreply.github.com>
2021-11-17 14:28:01 +0700

    Create happymod.js

---
+++ scraper/happymod.js
@@ -0,0 +1,29 @@
+const axios = require('axios')
+const cheerio = require('cheerio')
+
+const baseUrl = 'https://www.happymod.com/'
+
+function happymodSearch(query) {
+	return new Promise((resolve, reject) => {
+		axios.get(baseUrl+'search.html?q='+query).then(async res => {
+		var $ = cheerio.load(res.data)
+		const hasil = []
+		$("div.pdt-app-box").each(function(c, d) {
+			var title = $(d).find("a").text().trim();
+			var icon = $(d).find("img.lazy").attr('data-original');
+			var rating = $(d).find("span").text();
+			var link = baseUrl+$(d).find("a").attr('href');
+			hasil.push({
+				title,
+				icon,
+				link,
+				rating
+			})
+	})
+		resolve(hasil)
+		console.log(hasil)
+	}).catch(reject)
+})
+}
+
+module.exports = { happymodSearch }
