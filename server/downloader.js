eafdefc6266cf49f491b973b7142f38a4872bea8
Loli Protector <74448616+Arya-was@users.noreply.github.com>
2021-11-19 20:28:20 +0700

    Update downloader.js

--- server/downloader.js
+++ server/downloader.js
@@ -1,6 +1,7 @@
 const express = require('express')
 var router = express.Router();
 const hxz = require('hxz-api')
+const yts = require('yt-search')
 __path = process.cwd()
 const fs = require('fs')
 const { getBuffer } = require('../lib/function')
@@ -12,6 +13,7 @@ const { stickerDl } = require('../scraper/stickerpack')
 const { dl } = require('../scraper/aiovideodl')
 const { spotifydl } = require('../scraper/spotify')
 const { jooxdl, joox } = require('../scraper/joox')
+const { pixivDownload } = require('../scraper/pixiv')
 
 router.get('/tiktok', async(req, res) => {
 	var link = req.query.link
@@ -89,6 +91,35 @@ router.get('/youtube', async(req, res) => {
 	var hasil = await dl(link)
 	try {
 		res.json(hasil)
+	} catch(err) {
+		console.log(err)
+		res.json({ message: 'Ups, error' })
+	}
+})
+router.get('/play', async(req, res) => {
+	var link = req.query.query
+	if (!query) return res.json({ message: 'masukan parameter query' })
+	let results = await yts(query)
+  	let vid = results.all.find(video => video.seconds < 3600)
+	if (!vid) return res.json({ message: 'not found!'})
+	var hasil = await dl(vid.url)
+	try {
+		res.json(hasil)
+	} catch(err) {
+		console.log(err)
+		res.json({ message: 'Ups, error' })
+	}
+})
+router.get('/pixiv', async(req, res) => {
+	var id = req.query.id
+	var ext = req.query.ext
+	if (!id) return res.json({ message: 'masukan parameter id' })
+	if (!ext) return res.json({ message: 'masukan parameter ext' })
+	var hasil = await pixivDownload(id, ext)
+	try {
+		var data = await getBuffer(hasil)
+		await fs.writeFileSync(__path +'/tmp/image.jpg', data)
+   		await res.sendFile(__path +'/tmp/image.jpg')
 	} catch(err) {
 		console.log(err)
 		res.json({ message: 'Ups, error' })
