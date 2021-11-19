b840904d519ceef37b799b6447039e2bb39129de
Loli Protector <74448616+Arya-was@users.noreply.github.com>
2021-11-19 18:58:38 +0700

    Create pixiv.js

---
+++ scraper/pixiv.js
@@ -0,0 +1,23 @@
+const axios = require ('axios')
+
+function searchIlust(query) {
+	return new Promise((resolve, reject) => { 
+		axios.get('https://api.lolicon.app/setu/v2?&size=regular&num=100&keyword='+query).then(res => {
+			const result = res.data.data
+      if (result >.length < 1) {
+          throw 'Hasil tidak di temukan!'
+      } else {
+        resolve(result)
+      }
+		})
+	})
+}
+
+function pixivDownload(id, ext) {
+	return new Promise((resolve, reject) => {
+		const result = 'https://pixiv.cat/'+id+ext
+		resolve(result)
+	})
+}
+
+module.exports = { searchIlust, pixivDownload }
