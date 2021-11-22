59405efc819a6143b02bb1b7f39e5a5a1f1dd60b
Loli Protector <74448616+Arya-was@users.noreply.github.com>
2021-11-22 22:24:06 +0700

    Create anime.js

---
+++ server/anime.js
@@ -0,0 +1,28 @@
+const express = require('express')
+var router = express.Router();
+
+const mynimeku = require('../scraper/mynime')
+
+router.get('/mynimekuSearch', async(req, res) => {
+  var query = req.query.query
+  if (!query) return res.json({ message: 'masukan parameter query' })
+  var result = await mynimeku.Search(query)
+  if (result < 1) return res.json({ message: 'anime not found!' })
+  res.json(result)
+})
+
+router.get('/mynimekuDetail', async(req, res) => {
+  var link = req.query.link
+	if (!link) return res.json({ message: 'masukan parameter Link' })
+   var result = await mynimeku.animeDetail(link)
+   res.json(result)
+})
+
+router.get('/mynimekuDownload', async(req, res) => {
+  var link = req.query.link
+	if (!link) return res.json({ message: 'masukan parameter Link' })
+   var result = await mynimeku.downloadEps(link)
+   res.json(result)
+})
+
+module.exports = router
