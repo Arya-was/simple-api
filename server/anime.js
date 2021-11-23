179ec1059e371366dc3a2531c1174325fed500fa
Loli Protector <74448616+Arya-was@users.noreply.github.com>
2021-11-23 11:41:58 +0700

    Update anime.js

--- server/anime.js
+++ server/anime.js
@@ -42,10 +42,10 @@ router.get('/storyanime', async(req, res) => {
   let json = data[Math.floor(Math.random() * data.length)]
   var dl_link = await dl(json)
   const buffer = await getBuffer(dl_link.medias[0].url)
-  await fs.writeFileSync(__path +'/tmp/video.mp4', buffer)
-  await res.sendFile(__path +'/tmp/video.mp4')
+  await fs.writeFileSync(__path +`/tmp/${json}.mp4`, buffer)
+  await res.sendFile(__path +`/tmp/${json}.mp4`)
   await sleep(300)
-  await fs.unlinkSync(__path + '/tmp/video.mp4')
+  await fs.unlinkSync(__path + `/tmp/${json}.mp4`)
 })
 
 
