2435e04eed645c9c14d5e45f7b22a40cb618b300
Ichinose Honami <74448616+Arya-was@users.noreply.github.com>
2022-01-02 15:10:30 +0700

    Update igdl.js

--- scraper/igdl.js
+++ scraper/igdl.js
@@ -1,24 +1,60 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
 
-function igDownload(url) {
-	return new Promise((resolve, reject) => {
-		axios.request({
-			url: 'https://downloadgram.org/#downloadhere',
-			method: 'POST',
-			data: new URLSearchParams(Object.entries({url: url, submit: ''})),
-			headers: {
-				'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36'
-			}
-		}).then(res => {
-			const $ = cheerio.load(res.data)
-			const result = []
-			$('#downloadBox > a').each(function(a, b) {
-				result.push($(b).attr('href'))
-			})
-			resolve(result)
-		}).catch(reject)
-	})
+function igDownload(Url) {
+  return new Promise((resolve, reject) => {
+    axios.get('https://snapinsta.app/', {
+      headers: {
+        'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36'
+      }
+    }).then(res => {
+      const cookie = res.headers[`set-cookie`][0].replace('; path=/', '')
+      const data = {
+        url: Url,
+        action: 'post'
+      }
+      axios.request({
+        url: 'https://snapinsta.app/action.php',
+        method: 'post',
+        data: new URLSearchParams(Object.entries(data)),
+        headers: {
+          'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36',
+          'cookie': cookie
+        }
+      }).then(data => {
+        const $ = cheerio.load(data.data)
+        const result = []
+        $('div.row.download-box > div.col-md-4').each((a, b) => {
+          let link = $(b).find('div.download-items > div.download-items__btn > a.abutton').attr('href');
+          result.push(link)
+        })
+        resolve(result)
+      })
+    })
+  })
+}
+
+function igStalk(Username) {
+  return new Promise((resolve, reject) => {
+    axios.get('https://dumpor.com/v/'+Username, {
+      headers: {
+        "cookie": "_inst_key=SFMyNTY.g3QAAAABbQAAAAtfY3NyZl90b2tlbm0AAAAYWGhnNS1uWVNLUU81V1lzQ01MTVY2R0h1.fI2xB2dYYxmWqn7kyCKIn1baWw3b-f7QvGDfDK2WXr8",
+        "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36"
+      }
+    }).then(res => {
+      const $ = cheerio.load(res.data)
+      const result = {
+        profile: $('#user-page > div.user > div.row > div > div.user__img').attr('style').replace(/(background-image: url\(\'|\'\);)/gi, ''),
+        fullname: $('#user-page > div.user > div > div.col-md-4.col-8.my-3 > div > a > h1').text(),
+        username: $('#user-page > div.user > div > div.col-md-4.col-8.my-3 > div > h4').text(),
+        post: $('#user-page > div.user > div > div.col-md-4.col-8.my-3 > ul > li:nth-child(1)').text().replace(' Posts',''),
+        followers: $('#user-page > div.user > div > div.col-md-4.col-8.my-3 > ul > li:nth-child(2)').text().replace(' Followers',''),
+        following: $('#user-page > div.user > div > div.col-md-4.col-8.my-3 > ul > li:nth-child(3)').text().replace(' Following',''),
+        bio: $('#user-page > div.user > div > div.col-md-5.my-3 > div').text()
+      }
+      resolve(result)
+    })
+  })
 }
 
 function igStory(username) {
@@ -65,4 +101,4 @@ function igStory(username) {
 }
 
 module.exports = igDownload
-module.exports = { igStory }
+module.exports = { igStory, igStalk }
